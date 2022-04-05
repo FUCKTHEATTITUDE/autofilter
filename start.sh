3f83b27ba1f15bbf48827986c7c7e2bbd82cc010
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-04-05 15:15:11 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning Repo, Please Wait..."
-  git clone -b master https://github.com/Naveen-TG/Dhanush.git /Dhanush
+  git clone -b master https://github.com/Naveen-TG/Masterolic.git /Masterolic
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Dhanush
+  git clone $UPSTREAM_REPO /Masterolic
 fi
-cd /Dhanush 
+cd /Masterolic
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
