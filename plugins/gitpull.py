9760f09692ccb34918a8bb88c28a3a1ac23c5b63
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-04-16 21:10:55 +0530

    Create gitpull.py

---
+++ plugins/gitpull.py
@@ -0,0 +1,18 @@
+
+@run_async
+@dev_plus
+def gitpull(update: Update, context: CallbackContext):
+    sent_msg = update.effective_message.reply_text(
+        "Pulling all changes from remote and then attempting to restart.")
+    subprocess.Popen('git pull', stdout=subprocess.PIPE, shell=True)
+
+    sent_msg_text = sent_msg.text + "\n\nChanges pulled...I guess.. Restarting in "
+
+    for i in reversed(range(5)):
+        sent_msg.edit_text(sent_msg_text + str(i + 1))
+        sleep(1)
+
+    sent_msg.edit_text("Restarted.")
+
+    os.system('restart.bat')
+    os.execv('start.bat', sys.argv)
