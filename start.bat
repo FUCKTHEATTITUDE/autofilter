a67ada429a478a1bc3863f7b9d957ebc847e2ce3
CT - Nandha <89440790+Ctzfamily@users.noreply.github.com>
2022-04-16 21:04:03 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE Rashmika
+TITLE Naveen-TG
 :: Enables virtual env mode and then starts Vegeta
-env\scripts\activate.bat && py -m Rashmika
+env\scripts\activate.bat && py -m Naveen-TG
